026883323f71d08d85c7c8e9c28daf24cdc765c1
Nanaya Miki <nanayamiki@gmail.com>
2023-03-30 05:17:19 +0000

    added countLetters.js file. very similar to the previous question countOnly. very important to understand how to manipulate object as well as how to get value using obj square notation.

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🔥🔥🔥 Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`💤💤💤 Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const countLetters = function(str) {
+  let result = {};
+    for(let letter of str) {
+      if(letter !== ' ') {
+        if(result[letter]) {
+          result[letter] += 1;
+        }else {
+          result[letter] = 1
+        }
+      }
+    }
+    console.log('result', result)
+  return result;
+}
+
+const func = countLetters("lighthouse in the house");
+
+
+// TEST CODE
+assertEqual(func['l'], 1);
+assertEqual(func['h'], 4);
+assertEqual(func['e'], 3);
+
+
