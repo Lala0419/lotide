bd47f7be582a079ad4dd5e4fb30d95eeb02083f7
Nanaya Miki <nanayamiki@gmail.com>
2023-04-01 16:59:38 +0000

    added eqObject.js file. struggled a lot on how to make each conditinals.

--- eqObjects.js
+++ eqObjects.js
@@ -20,26 +20,36 @@ const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] }
 const longSleeveMultiColorShirtObject= { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
 
 const eqObjects = function(object1, object2) {
-    if(Object.keys(object1).length  === Object.keys(object2).length) {
-      for(let key of Object.keys(object1)) {
-        if(Array.isArray(object1[key]) || Array.isArray(object2[key])) {
-          eqArrays(object1[key], object2[key])
-        }
-          if(object1[key] === object2[key]) {
-            return true
-          }
-          return false
-          }
-        
+  if(Object.keys(object1).length  !== Object.keys(object2).length) {
+    return false
   }
+  for(let key in object1) {
+        // console.log('key',key)
+    if(object2.hasOwnProperty(key) === false){ //checking if they have the same key
+      return false
+    }else {
+          // object1[key] === colors: ["red", "blue"]
+      if(Array.isArray(object1[key])) { // chacking the key' value is an array or not
+        const theseArrIsEqual = eqArrays(object1[key], object2[key]) //checking the key's value
+        if(theseArrIsEqual === false) {
+          return false
+        }
+      }else if(object1[key] !== object2[key]) {
+        return false
+      }
+        // if(Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+        //   eqArrays(object1[key], object2[key])
+        // }
+    }
+    return true  //whenever you are kicked out from the condition, you come here. 
+  }
+}
 
- return false
-};
- eqObjects(shirtObject , anotherShirtObject); // => true
-
+//  eqObjects(shirtObject , anotherShirtObject); // => true
+console.log(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject))
 // TEST CODE
-assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
-assertEqual(eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject)); // => true
-assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject)); // => false
+ assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+ assertEqual(eqObjects(multiColorShirtObject  , longSleeveMultiColorShirtObject), false); // => false
 
 //?? Not handring the array well. so the line 42, 43 is false and undefined/
