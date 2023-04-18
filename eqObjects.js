27cbabb64ce927d9264b8c698ac94c3ae0d03737
Nanaya Miki <nanayamiki@gmail.com>
2023-04-18 13:21:48 +0000

    added more test cases and fixed the eqObjects func accordingly.

--- eqObjects.js
+++ eqObjects.js
@@ -4,7 +4,11 @@ const eqArrays = require("./eqArrays");
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
-
+const longSleeveShirtObject = {
+	size: "medium",
+	color: "red",
+	sleeveLength: "long",
+};
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = {
 	size: "medium",
@@ -17,33 +21,38 @@ const longSleeveMultiColorShirtObject = {
 };
 
 const eqObjects = function (object1, object2) {
-	if (Object.keys(object1).length !== Object.keys(object2).length) {
+	const obj1Keys = Object.keys(object1);
+	const obj2Keys = Object.keys(object2);
+
+	if (obj1Keys.length !== obj2Keys.length) {
 		return false;
 	}
-	for (const key of Object.keys(object1)) {
-		if (object2.hasOwnProperty(key) === false) {
+
+	for (const key of obj1Keys) {
+		if (!object2.hasOwnProperty(key)) {
 			return false;
-		} else {
-			if (Array.isArray(object1[key])) {
-				// chacking the key' value is an array or not
-				const theseArrIsEqual = eqArrays(object1[key], object2[key]);
-				//checking the key's value
-				if (theseArrIsEqual === false) {
-					return false;
-				}
-			} else if (
-				typeof object1[key] === "object" &&
-				typeof object2[key] === "object"
-			) {
-				return eqObjects(object1[key], object2[key]);
-			} else if (object1[key] !== object2[key]) {
-				//key value is not an array so just checking if it is an imprimitive value
+		}
+
+		const value1 = object1[key];
+		const value2 = object2[key];
+		if (value1 === null || value2 === null) {
+			if (value1 !== vaue2) {
 				return false;
 			}
+		} else if (Array.isArray(value1) && Array.isArray(value2)) {
+			if (!eqArrays(value1, value2)) {
+				return false;
+			}
+		} else if (typeof value1 === "object" && typeof value2 === "object") {
+			if (!eqObjects(value1, value2)) {
+				return false;
+			}
+		} else if (value1 !== value2) {
+			return false;
 		}
-		return true;
-		//whenever you are kicked out from the condition, you come here.
 	}
+
+	return true;
 };
 
 assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
@@ -55,5 +64,12 @@ assertEqual(
 	eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject),
 	false
 );
+console.log(eqObjects({ a: 1, b: 2 }, { b: 1, a: 1 })); // false
+console.log(eqObjects({ a: 1, b: [2, 3] }, { a: 1, b: [2, 4] })); // false
+console.log(eqObjects({ a: [1, 2], b: 2 }, { a: [1, 2], b: 3 })); // false
+console.log(eqObjects(shirtObject, longSleeveShirtObject)); // => false
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
 
 module.exports = eqObjects;
