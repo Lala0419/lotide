8f477d84219e6db02302a68f39e3db9894156e97
Nanaya Miki <nanayamiki@gmail.com>
2023-04-18 06:09:46 +0000

    fixed flatten func from using library func flat to recursion

--- flatten.js
+++ flatten.js
@@ -1,7 +1,16 @@
 const eqArrays = require("./eqArrays");
 
 const flatten = function (arr) {
-	return arr.flat(Infinity);
+	let result = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (Array.isArray(arr[i])) {
+			result = result.concat(flatten(arr[i]));
+		} else {
+			result.push(arr[i]);
+		}
+	}
+
+	return result;
 };
 
 eqArrays(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
